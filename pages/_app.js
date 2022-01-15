281b1c68a545a277e55e895f4456840f92f9f449
bluehorn07 <bluehornblues@gmail.com>
2022-01-15 13:37:04 +0900

    remove not using packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import '../styles/font.scss';
 import 'semantic-ui-css/semantic.min.css';
 import '../styles/globals.scss';
-import 'react-datepicker/dist/react-datepicker.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;

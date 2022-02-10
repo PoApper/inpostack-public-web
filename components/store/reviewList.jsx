759256c6c06aa78f46f357900f53641fce98e3df
bluehorn07 <bluehornblues@gmail.com>
2022-02-10 21:18:30 +0900

    separate user review form

--- components/store/reviewList.jsx
+++ components/store/reviewList.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Button, Comment, Divider, Form, Header } from 'semantic-ui-react'
+import { Comment, Divider, Header } from 'semantic-ui-react'
 import moment from 'moment'
 import styled from 'styled-components'
+import UserReviewForm from './userReviewForm'
 
 const ReviewList = (props) => {
   const [reviewList, setReviewList] = useState([])
-  const [userReview, setUserReview] = useState()
   const store = props.store
 
   useEffect(() => {
@@ -16,25 +16,6 @@ const ReviewList = (props) => {
       .then(res => setReviewList(res.data))
       .catch(() => alert(`리뷰 목록을 불러오는데 실패했습니다.`))
   }, [store])
-
-  const submitReview = async () => {
-    try {
-      await axios.post(`${process.env.NEXT_PUBLIC_API}/review/reviewer`, {
-        content: userReview,
-        store_uuid: store.uuid,
-      }, { withCredentials: true })
-      alert('리뷰가 등록되었습니다!')
-      window.location.reload()
-    } catch (err) {
-      const status = err.response.status
-      if (status === 403) {
-        alert('로그인 후 리뷰를 이용해주세요!')
-      } else {
-        alert('리뷰 작성에 실패했습니다.')
-      }
-      console.log(err)
-    }
-  }
 
   return (
     <CommentDiv>
@@ -68,13 +49,8 @@ const ReviewList = (props) => {
 
       <Divider/>
 
-      <Form reply onSubmit={submitReview}>
-        <Form.TextArea
-          placeholder={'인포스택을 이용하고, 리뷰를 남겨보세요! 😁'}
-          onChange={e => setUserReview(e.target.value)}
-        />
-        <Button content="리뷰 게시" labelPosition="left" icon="edit" primary/>
-      </Form>
+      <UserReviewForm store={store}/>
+
     </CommentDiv>
   )
 }

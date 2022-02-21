173351c0ae2c4c1e00ab993a600edf2ee911fb6f
hodori314 <hodori@postech.ac.kr>
2022-02-21 20:55:21 +0900

    separate storeTypeList

--- pages/store/index.jsx
+++ pages/store/index.jsx
@@ -4,6 +4,75 @@ import axios from 'axios'
 import styled from 'styled-components'
 import { Image } from 'semantic-ui-react'
 import Layout from '../../components/layout'
+import StoreTypeList from '../../components/store/storeTypeList'
+
+const storeList = [
+  {
+    storeType: "all",
+    storeLogo: "store_logo/all.svg",
+    description: "전체보기",
+  },
+  {
+    storeType: "KOREAN_STEW",
+    storeLogo: "/store_logo/된장찌개.svg",
+    description: "찌개",
+  },
+  {
+    storeType: "DESSERT",
+    storeLogo: "/store_logo/디저트.svg",
+    description: "디저트",
+  },
+  {
+    storeType: "SNACK_SHOP",
+    storeLogo: "/store_logo/떡볶이.svg",
+    description: "분식",
+  },
+  {
+    storeType: "SEA_FOOD",
+    storeLogo: "/store_logo/물고기.svg",
+    description: "해산물",
+  },
+  {
+    storeType: "KOREAN",
+    storeLogo: "store_logo/밥.svg",
+    description: "정식",
+  },
+  {
+    storeType: "BOSSAM",
+    storeLogo: "/store_logo/보쌈.svg",
+    description: "고기/보쌈",
+  },
+  {
+    storeType: "WESTERN",
+    storeLogo: "/store_logo/스테이크.svg",
+    description: "양식",
+  },
+  {
+    storeType: "CHINESE",
+    storeLogo: "/store_logo/짜장면.svg",
+    description: "중식",
+  },
+  {
+    storeType: "JAPANESE",
+    storeLogo: "/store_logo/초밥.svg",
+    description: "일식",
+  },
+  {
+    storeType: "CHICKEN",
+    storeLogo: "/store_logo/치킨.svg",
+    description: "치킨",
+  },
+  {
+    storeType: "PIZZA",
+    storeLogo: "/store_logo/피자.svg",
+    description: "피자",
+  },
+  {
+    storeType: "HAMBURGER",
+    storeLogo: "/store_logo/햄버거.svg",
+    description: "햄버거",
+  }
+]
 
 const StoreIndexPage = () => {
   const [stores, setStores] = useState([])
@@ -20,58 +89,16 @@ const StoreIndexPage = () => {
     <Layout>
       <Title>가게 목록</Title>
       <StoreTypesList>
-        <LogoDiv onClick={() => setStoreType('all')}>
-          <StoreLogoImg src={'store_logo/all.svg'}/>
-          <p>전체보기</p>
-        </LogoDiv>
-        <LogoDiv onClick={() => setStoreType('KOREAN_STEW')}>
-          <StoreLogoImg src={'/store_logo/된장찌개.svg'}/>
-          <p>찌개</p>
-        </LogoDiv>
-        <LogoDiv onClick={() => setStoreType('DESSERT')}>
-          <StoreLogoImg src={'/store_logo/디저트.svg'}/>
-          <p>디저트</p>
-        </LogoDiv>
-        <LogoDiv onClick={() => setStoreType('SNACK_SHOP')}>
-          <StoreLogoImg src={'/store_logo/떡볶이.svg'}/>
-          <p>분식</p>
-        </LogoDiv>
-        <LogoDiv onClick={() => setStoreType('SEA_FOOD')}>
-          <StoreLogoImg src={'/store_logo/물고기.svg'}/>
-          <p>해산물</p>
-        </LogoDiv>
-        <LogoDiv onClick={() => setStoreType('KOREAN')}>
-          <StoreLogoImg src={'/store_logo/밥.svg'}/>
-          <p>정식</p>
-        </LogoDiv>
-        <LogoDiv onClick={() => setStoreType('BOSSAM')}>
-          <StoreLogoImg src={'/store_logo/보쌈.svg'}/>
-          <p>고기/보쌈</p>
-        </LogoDiv>
-        <LogoDiv onClick={() => setStoreType('WESTERN')}>
-          <StoreLogoImg src={'/store_logo/스테이크.svg'}/>
-          <p>양식</p>
-        </LogoDiv>
-        <LogoDiv onClick={() => setStoreType('CHINESE')}>
-          <StoreLogoImg src={'/store_logo/짜장면.svg'}/>
-          <p>중식</p>
-        </LogoDiv>
-        <LogoDiv onClick={() => setStoreType('JAPANESE')}>
-          <StoreLogoImg src={'/store_logo/초밥.svg'}/>
-          <p>일식</p>
-        </LogoDiv>
-        <LogoDiv onClick={() => setStoreType('CHICKEN')}>
-          <StoreLogoImg src={'/store_logo/치킨.svg'}/>
-          <p>치킨</p>
-        </LogoDiv>
-        <LogoDiv onClick={() => setStoreType('PIZZA')}>
-          <StoreLogoImg src={'/store_logo/피자.svg'}/>
-          <p>피자</p>
-        </LogoDiv>
-        <LogoDiv onClick={() => setStoreType('HAMBURGER')}>
-          <StoreLogoImg src={'/store_logo/햄버거.svg'}/>
-          <p>햄버거</p>
-        </LogoDiv>
+      {
+        storeList.map(store => {
+          return(
+            <StoreTypeList onClick={() => setStoreType(store.storeType)}
+                          storeLogo={store.storeLogo} description={store.description}
+                          key={store.description}>
+            </StoreTypeList>
+          )
+        })
+      }
       </StoreTypesList>
       <StoreGrid>
         {
@@ -123,16 +150,6 @@ const StoreTypesList = styled.div`
   @media only screen and (max-width: ${({ theme }) => theme.breakpoint.s}) {
     grid-template-columns: repeat(5, 1fr);
   }
-`
-
-const LogoDiv = styled.div`
-  text-align: center;
-  cursor: pointer;
-`
-
-const StoreLogoImg = styled.img`
-  width: 48px;
-  height: 48px;
 `
 
 const StoreGrid = styled.div`

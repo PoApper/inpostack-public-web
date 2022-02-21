f411dd9108f4a760e5b620f11fd84579584530cf
bluehorn07 <bluehornblues@gmail.com>
2022-02-21 22:27:35 +0900

    update blur text

--- pages/store/[store_name].jsx
+++ pages/store/[store_name].jsx
@@ -34,7 +34,7 @@ const StorePage = () => {
               <p>
                 InPoStack은 포항공대 학생들의 제보로 운영되는 맛집 사이트 입니다. 🍴
                 운영에 최선을 다하고 있지만 가게와 메뉴의 최신 정보를 반영하지 못 할 수도 있습니다. 🙏
-                가게·메뉴 정보에서 이렇게 <span className={"blurry-text"}>포항공대</span> 흐릿하게
+                가게·메뉴 정보에서 <span className={"blurry-text"}>정보 수집중</span> 흐릿하게
                 표시된 부분은 InPoStack 맛집 데이터베이스에 정보가 없는 항목 입니다. 😢
                 &ldquo;<a href={process.env.NEXT_PUBLIC_REQUEST_GOOGLE_FORM_URL}
                           target={"_blank"} rel={'noreferrer'}>

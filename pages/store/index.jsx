5fc3b384eeb1f59fea76876adc1b6c16beaae257
bluehorn07 <bluehornblues@gmail.com>
2022-03-10 21:39:21 +0900

    create store-type asset and polish codes

--- pages/store/index.jsx
+++ pages/store/index.jsx
@@ -1,80 +1,14 @@
 import React, { useEffect, useState } from 'react'
+import Head from 'next/head'
 import Link from 'next/link'
 import axios from 'axios'
 import styled from 'styled-components'
 import { Dropdown, Image, Label } from 'semantic-ui-react'
 import Layout from '../../components/layout'
-import StoreTypeList from '../../components/store/storeTypeList'
-import Head from 'next/head'
+import StoreTypeLogo from '../../components/store/storeTypeLogo'
 import { textLengthOverCut } from '../../utils/text-length-over-cut'
 
-const storeList = [
-  {
-    storeType: 'all',
-    storeLogo: 'store_logo/all.svg',
-    description: '전체보기',
-  },
-  {
-    storeType: 'KOREAN_STEW',
-    storeLogo: '/store_logo/된장찌개.svg',
-    description: '찌개',
-  },
-  {
-    storeType: 'DESSERT',
-    storeLogo: '/store_logo/디저트.svg',
-    description: '디저트',
-  },
-  {
-    storeType: 'SNACK_SHOP',
-    storeLogo: '/store_logo/떡볶이.svg',
-    description: '분식',
-  },
-  {
-    storeType: 'SEA_FOOD',
-    storeLogo: '/store_logo/물고기.svg',
-    description: '해산물',
-  },
-  {
-    storeType: 'KOREAN',
-    storeLogo: 'store_logo/밥.svg',
-    description: '정식',
-  },
-  {
-    storeType: 'BOSSAM',
-    storeLogo: '/store_logo/보쌈.svg',
-    description: '고기/보쌈',
-  },
-  {
-    storeType: 'WESTERN',
-    storeLogo: '/store_logo/스테이크.svg',
-    description: '양식',
-  },
-  {
-    storeType: 'CHINESE',
-    storeLogo: '/store_logo/짜장면.svg',
-    description: '중식',
-  },
-  {
-    storeType: 'JAPANESE',
-    storeLogo: '/store_logo/초밥.svg',
-    description: '일식',
-  },
-  {
-    storeType: 'CHICKEN',
-    storeLogo: '/store_logo/치킨.svg',
-    description: '치킨',
-  },
-  {
-    storeType: 'PIZZA',
-    storeLogo: '/store_logo/피자.svg',
-    description: '피자',
-  },
-  {
-    storeType: 'HAMBURGER',
-    storeLogo: '/store_logo/햄버거.svg',
-    description: '햄버거',
-  },
-]
+import StoreTypeList from '../../assets/StoreTypeList'
 
 const StoreIndexPage = () => {
   const [stores, setStores] = useState([])
@@ -107,11 +41,11 @@ const StoreIndexPage = () => {
 
       <StoreTypesList>
         {
-          storeList.map(store => {
+          StoreTypeList.map(store => {
             return (
               <StoreCheck key={store.description}
                           onClick={() => setStoreType(store.storeType)}>
-                <StoreTypeList storeLogo={store.storeLogo}
+                <StoreTypeLogo storeLogo={store.storeLogo}
                                description={store.description}
                                storeType={store.storeType}/>
               </StoreCheck>
